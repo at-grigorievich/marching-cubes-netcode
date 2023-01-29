9c0cde75bfc407c5398a46116a74f22afb3796bf
at-grigorievich <at.grigorievich@gmail.com>
2023-01-29 15:53:25 +0300

    upd mathf helper

--- MarchingCubes-Netcode/Assets/Scripts/Mine Generator/MathfHelper.cs
+++ MarchingCubes-Netcode/Assets/Scripts/Mine Generator/MathfHelper.cs
@@ -19,11 +19,7 @@ namespace MineGenerator
         
         public static bool IsPointInRadius(Vector3 point, Vector3 center, float Radius, out float distance)
         {
-            float x = point.x - center.x;
-            float y = point.y - center.y;
-            float z = point.z - center.z;
-
-            distance = Mathf.Sqrt(x * x + y * y + z * z);
+            distance = Distance(point,center);
 
             return distance - Radius <= 0f;
         }
@@ -38,6 +34,29 @@ namespace MineGenerator
             bool isZ = point.z >= leftBottom.z && point.z <= rightTop.z;
 
             return isX && isY && isZ;
+        }
+        
+        public static bool IsPointInCube(Vector3 point, Vector3 center, float radius, out float distance)
+        {
+            Vector3 leftBottom = center - new Vector3(1f, 1f, 1f) * radius;
+            Vector3 rightTop = center + new Vector3(1f, 1f, 1f) * radius;
+
+            bool isX = point.x >= leftBottom.x && point.x <= rightTop.x;
+            bool isY = point.y >= leftBottom.y && point.y <= rightTop.y;
+            bool isZ = point.z >= leftBottom.z && point.z <= rightTop.z;
+
+            distance = Distance(point, center);
+            
+            return isX && isY && isZ;
+        }
+
+        public static float Distance(Vector3 first, Vector3 second)
+        {
+            float x = first.x - second.x;
+            float y = first.y - second.y;
+            float z = first.z - second.z;
+
+            return Mathf.Sqrt(x * x + y * y + z * z);
         }
     }
 }
